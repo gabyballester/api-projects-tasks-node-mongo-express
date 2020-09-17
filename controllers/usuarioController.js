30554961e8576d09ac759a21d1c2ceecadd86483
gabyballester <gaby.ballester@gmail.com>
2020-09-17 21:26:07 +0200

    Creación de jwt de datos de usuario

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,16 +1,24 @@
 const Usuario = require('../models/UserModel');
 const bcryptjs = require('bcryptjs');
+const { validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken')
 
 exports.crearUsuario = async (req, res) => {
+
+    // revisar si hay errores 
+    const errores = validationResult(req);
+    if (!errores.isEmpty()) {
+        return res.status(400).json({ errores: errores.array() })
+    }
 
     // extraer email y password
     const { email, password } = req.body;
 
     try {
         //comprobar usuario único
-        let usuario = await Usuario.findOne({email});
-        if(usuario){
-            return res.status(400).json({msg: 'Usuario ya existe'});
+        let usuario = await Usuario.findOne({ email });
+        if (usuario) {
+            return res.status(400).json({ msg: 'Usuario ya existe' });
         }
         // crea el nuevo usuario
         usuario = new Usuario(req.body)
@@ -19,7 +27,21 @@ exports.crearUsuario = async (req, res) => {
         usuario.password = await bcryptjs.hash(password, salt);
         //guarda usuario
         await usuario.save()
-        res.status(200).json({msg: 'Usuario creado correctamente'});
+        // Crear jwt con datos usuario
+        const payload = {
+            usuario: {
+                id: usuario.id,
+            }
+        }
+        // Firmar jwt
+        jwt.sign(payload, process.env.SECRETA, {
+            expiresIn: 3600 //1 hora
+        }, (error, token) => {
+            // si hay error
+            if (error) throw error;
+            // mensaje de confirmación
+            res.json({ token });
+        })
     } catch (error) {
         console.log(error);
         res.status(400).send('Hubo un error');
