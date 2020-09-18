cf042a2787578f810f4686b20b93314431fb6bd9
gabyballester <gaby.ballester@gmail.com>
2020-09-18 22:40:51 +0200

    Aplicando validación para formulario de proyectos

--- controllers/proyectoController.js
+++ controllers/proyectoController.js
@@ -1,6 +1,12 @@
 const Proyecto = require('../models/ProjectModel');
+const { validationResult } = require('express-validator');
 
 exports.crearProyecto = async (req, res) => {
+    //Revisar si hay errores
+    const errores = validationResult(req);
+    if (!errores.isEmpty()) {
+        return res.status(400).json({ errores: errores.array() })
+    }
     try {
         // Crear un nuevo proyecto
         const proyecto = new Proyecto(req.body)
--- routes/proyectosRoutes.js
+++ routes/proyectosRoutes.js
@@ -2,11 +2,15 @@ const express = require('express');
 const router = express.Router();
 const proyectoController = require('../controllers/proyectoController');
 const auth = require('../middlewares/auth');
+const { check } = require('express-validator');
 
 // Crea proyectos
 // api/proyectos
 router.post('/',
-    auth, proyectoController.crearProyecto
+    auth,
+    [
+        check('nombre', 'El nombre del proyecto es obligatorio').not().isEmpty()
+    ], proyectoController.crearProyecto
 )
 
 router.get('/',
